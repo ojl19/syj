21becaca17f299e2c0087eca754b9de4f8c3a8a4
ojl19 <qq1850402389@qq.com>
2022-07-31 20:28:12 +0800

    项目初始化

---
+++ src/main/java/com/tf/syj/SyjApplication.java
@@ -0,0 +1,13 @@
+package com.tf.syj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tf/syj/SyjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tf.syj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
